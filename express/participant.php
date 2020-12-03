68612370c4e60b434e41de925925ac80bf98c0d8
Schnieder Maxime <schniedermaxime@Schnieders-MacBook-Air.local>
2020-12-02 23:47:11 -0500

    final commit updated

--- express/participant.php
+++ express/participant.php
@@ -90,36 +90,33 @@
 						<img src="https://www.w3schools.com/w3images/newyork.jpg" alt="New York" style="width:100%"
 							class="w3-hover-opacity">
 						<div class="w3-container w3-white">
-							<p><b>New York</b></p>
+							<p><b>Event Location</b></p>
 							<p class="w3-opacity">Fri 27 Nov 2016</p>
-							<p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+							<p>Event Description</p>
 							<button class="w3-button w3-black w3-margin-bottom"
-								onclick="document.getElementById('ticketModal').style.display='block'">Buy
-								Tickets</button>
+								onclick="document.getElementById('ticketModal').style.display='block'">View Event</button>
 						</div>
 					</div>
 					<div class="w3-third w3-margin-bottom">
 						<img src="https://www.w3schools.com/w3images/paris.jpg" alt="Paris" style="width:100%"
 							class="w3-hover-opacity">
 						<div class="w3-container w3-white">
-							<p><b>Paris</b></p>
+							<p><b>Event Location</b></p>
 							<p class="w3-opacity">Sat 28 Nov 2016</p>
-							<p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+							<p>Event Description</p>
 							<button class="w3-button w3-black w3-margin-bottom"
-								onclick="document.getElementById('ticketModal').style.display='block'">Buy
-								Tickets</button>
+								onclick="document.getElementById('ticketModal').style.display='block'">View Event</button>
 						</div>
 					</div>
 					<div class="w3-third w3-margin-bottom">
 						<img src="https://www.w3schools.com/w3images/sanfran.jpg" alt="San Francisco" style="width:100%"
 							class="w3-hover-opacity">
 						<div class="w3-container w3-white">
-							<p><b>San Francisco</b></p>
+							<p><b>Event Location</b></p>
 							<p class="w3-opacity">Sun 29 Nov 2016</p>
-							<p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+							<p>Event Description</p>
 							<button class="w3-button w3-black w3-margin-bottom"
-								onclick="document.getElementById('ticketModal').style.display='block'">Buy
-								Tickets</button>
+								onclick="document.getElementById('ticketModal').style.display='block'">View Event</button>
 						</div>
 					</div>
 				</div>
